21d223a3a29d9770ae7cde6387da9c10bcddbd6b
Maciej Drobot <maciejdrobot@gmail.com>
2019-05-22 23:21:06 +0100

    Plansza - metody + Gra(runner)

---
+++ src/main/java/com/sda/algorytmy/Gra.java
@@ -0,0 +1,23 @@
+package com.sda.algorytmy;
+
+import java.util.Scanner;
+
+public class Gra {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        Plansza plansza1 = new Plansza();
+        plansza1.utworzPlansze();
+        Plansza plansza2 = new Plansza();
+        plansza2.utworzPlansze();
+
+        //Gracz1 rozstaw statki - set plansza1
+        //Drukuj plansze ze statkami
+        //Gracz2 rozstaw statki - set plansza2
+        //Drukuj plansze ze statkami
+
+
+
+
+    }
+}
--- src/main/java/com/sda/algorytmy/Plansza.java
+++ src/main/java/com/sda/algorytmy/Plansza.java
@@ -1,6 +1,53 @@
 package com.sda.algorytmy;
 
 public class Plansza {
+    char[][] plansza = new char[11][11];
 
+    public char getPlansza(int i, int j) {
+        return plansza[i][j];
+    }
 
+//    public void setPlansza(char[][] plansza) {
+//        this.plansza = plansza;
+//    }
+
+    public void utworzPlansze() {
+        for (int i = 0; i < 11; i++) {
+            for (int j = 0; j < 11; j++) {
+                if (i == 0) {
+                    plansza[i][j] = (char) (48 + j);
+                } else if (j == 0) {
+                    plansza[i][j] = (char) (64 + i);
+                } else {
+                    plansza[i][j] = '0';
+                }
+                plansza[0][10] = '1';
+                plansza[0][0] = ' ';
+            }
+        }
+    }
+
+    public void drukujPlansze() {
+        for (int i = 0; i < 11; i++) {
+            for (int j = 0; j < 11; j++) {
+                //Wyjatek zeby nie drukowac statkow
+                if (plansza[i][j] == 'S') {
+                    System.out.print('0' + " ");
+                } else {
+                    System.out.print(plansza[i][j] + " ");
+                }
+            } System.out.println();
+        }
+    }
+
+    public void drukujPlanszeStatki() {
+        for (int i = 0; i < 11; i++) {
+            for (int j = 0; j < 11; j++) {
+                System.out.print(plansza[i][j] + " ");
+            }
+        } System.out.println();
+    }
 }
+
+
+
