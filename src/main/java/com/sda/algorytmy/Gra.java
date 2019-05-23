2bce5eb143466345224cbbe729584633ce41d46b
Maciej Drobot <maciejdrobot@gmail.com>
2019-05-23 23:16:25 +0100

    Klasa Ruch - pierwsze metody

--- src/main/java/com/sda/algorytmy/Gra.java
+++ src/main/java/com/sda/algorytmy/Gra.java
@@ -6,23 +6,29 @@ public class Gra {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        //Poczatek gry - tworzenie planszy
         Plansza plansza1 = new Plansza();
         plansza1.utworzPlansze();
         Plansza plansza2 = new Plansza();
         plansza2.utworzPlansze();
 
+        //Ustawianie statkow
+        Ruch.ustawianieStatkow(plansza1, scanner, 1);
+        Ruch.ustawianieStatkow(plansza2, scanner, 2);
 
-       plansza1.ustawStatek(5, 5);
-       plansza1.zaznaczPudlo(4,4);
-       plansza1.zaznaczTrafiony(3,3);
 
-        plansza1.drukujPlanszeStatki();
+        //Strzelanie
+        System.out.println("Gracz 1 strzela:");
+        plansza1.drukujPlansze();
+        String strzal = scanner.next();
+        if (plansza1.getPlansza(Ruch.wspI(strzal), Ruch.wspJ(strzal)) == 'S') {
+            System.out.println("Trafiony!");
+            plansza1.zaznaczTrafiony(Ruch.wspI(strzal), Ruch.wspJ(strzal));
+        } else {
+            System.out.println("Pudlo!");
+            plansza1.zaznaczPudlo(Ruch.wspI(strzal), Ruch.wspJ(strzal));
 
-        //Gracz1 rozstaw statki - set plansza1
-        //Drukuj plansze ze statkami
-        //Gracz2 rozstaw statki - set plansza2
-        //Drukuj plansze ze statkami
-
+        }
 
 
 
--- src/main/java/com/sda/algorytmy/Ruch.java
+++ src/main/java/com/sda/algorytmy/Ruch.java
@@ -1,22 +1,33 @@
 package com.sda.algorytmy;
 
+import java.util.Scanner;
+
 public class Ruch {
 
-    public static int wspolrzednaI(String strzal) {
+    public static int wspJ(String strzal) {
         char wspolrzedne[] = strzal.toCharArray();
-        int i;
+        int j;
         if (wspolrzedne.length == 2) {
-            i = wspolrzedne[1] - 48;
+            j = wspolrzedne[1] - 49;
         } else {
-            i = 10;
+            j = 10;
         }
-        return i;
-    }
-
-    public static int wspolrzednaJ(String strzal) {
-        char wspolrzedne[] = strzal.toCharArray();
-        int j = wspolrzedne[0] - 96;
         return j;
     }
 
+    public static int wspI(String strzal) {
+        char wspolrzedne[] = strzal.toCharArray();
+        int i = wspolrzedne[0] - 97;
+        return i;
+    }
+
+    public static void ustawianieStatkow(Plansza plansza, Scanner scanner, int gracz) {
+        System.out.println("Gracz " + gracz + " - Rozstaw swoje statki:");
+        plansza.drukujPlansze();
+        for (int i = 0; i < 2; i++) {
+            String statek = scanner.next();
+            plansza.ustawStatek(Ruch.wspI(statek), Ruch.wspJ(statek));
+
+        }
+    }
 }
