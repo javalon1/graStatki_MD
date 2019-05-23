a45454302c2ebf6da5624722e67c8c26e2023201
Maciej Drobot <maciejdrobot@gmail.com>
2019-05-23 21:39:00 +0100

    Plansza - proste tworzenie, drukowanie poprawione

--- src/main/java/com/sda/algorytmy/Plansza.java
+++ src/main/java/com/sda/algorytmy/Plansza.java
@@ -1,7 +1,7 @@
 package com.sda.algorytmy;
 
 public class Plansza {
-    char[][] plansza = new char[10][11];
+    char[][] plansza = new char[10][10];
 
     public char getPlansza(int i, int j) {
         return plansza[i][j];
@@ -13,20 +13,17 @@ public class Plansza {
 
     public void utworzPlansze() {
         for (int i = 0; i < 10; i++) {
-            for (int j = 0; j < 11; j++) {
-                if (j == 0) {
-                    plansza[i][j] = (char) (65 + i);
-                } else {
+            for (int j = 0; j < 10; j++) {
                     plansza[i][j] = '0';
                 }
             }
         }
-    }
 
     public void drukujPlansze() {
         System.out.println("  1 2 3 4 5 6 7 8 9 10");
         for (int i = 0; i < 10; i++) {
-            for (int j = 0; j < 11; j++) {
+            System.out.print((char) (65 + i) + " ");
+            for (int j = 0; j < 10; j++) {
                 //Wyjatek zeby nie drukowac statkow
                 if (plansza[i][j] == 'S') {
                     System.out.print('0' + " ");
@@ -41,7 +38,8 @@ public class Plansza {
     public void drukujPlanszeStatki() {
         System.out.println("  1 2 3 4 5 6 7 8 9 10");
         for (int i = 0; i < 10; i++) {
-            for (int j = 0; j < 11; j++) {
+            System.out.print((char) (65 + i) + " ");
+            for (int j = 0; j < 10; j++) {
                 System.out.print(plansza[i][j] + " ");
             }
             System.out.println();
