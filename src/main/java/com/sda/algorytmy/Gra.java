75ea00fb22c5370c15bf5ac17c488c47fcc82d9d
Maciej Drobot <maciejdrobot@gmail.com>
2019-05-25 21:24:00 +0100

    Strzelanie - pierwsza petla dziala

--- src/main/java/com/sda/algorytmy/Gra.java
+++ src/main/java/com/sda/algorytmy/Gra.java
@@ -6,6 +6,7 @@ public class Gra {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+
         //Poczatek gry - tworzenie planszy
         Plansza plansza1 = new Plansza();
         plansza1.utworzPlansze();
@@ -18,18 +19,34 @@ public class Gra {
 
 
         //Strzelanie
-        System.out.println("Gracz 1 strzela:");
-        plansza1.drukujPlansze();
-        String strzal = scanner.next();
-        if (plansza1.getPlansza(Ruch.wspI(strzal), Ruch.wspJ(strzal)) == 'S') {
-            System.out.println("Trafiony!");
-            plansza1.zaznaczTrafiony(Ruch.wspI(strzal), Ruch.wspJ(strzal));
-        } else {
-            System.out.println("Pudlo!");
-            plansza1.zaznaczPudlo(Ruch.wspI(strzal), Ruch.wspJ(strzal));
+        int trafieniaG1 = 0;
+        int trafieniaG2 = 0;
 
-        }
+        Ruch.strzelanie(plansza2, scanner, 1, trafieniaG1);
+        Ruch.strzelanie(plansza1, scanner, 2, trafieniaG2);
 
+
+
+
+
+
+
+
+
+
+//        System.out.println("Gracz " + gracz + " strzela:");
+//        plansza1.drukujPlansze();
+//        String strzal = scanner.next();
+//        if (plansza2.getPlansza(Ruch.wspI(strzal), Ruch.wspJ(strzal)) == 'S') {
+//            System.out.println("Trafiony!");
+//            plansza2.zaznaczTrafiony(Ruch.wspI(strzal), Ruch.wspJ(strzal));
+//            trafieniaG1++;
+//            kolejka = false;
+//        } else {
+//            System.out.println("Pudlo!");
+//            plansza2.zaznaczPudlo(Ruch.wspI(strzal), Ruch.wspJ(strzal));
+//            kolejka = true;
+//        }
 
 
     }
--- src/main/java/com/sda/algorytmy/Ruch.java
+++ src/main/java/com/sda/algorytmy/Ruch.java
@@ -4,6 +4,8 @@ import java.util.Scanner;
 
 public class Ruch {
 
+
+
     public static int wspJ(String strzal) {
         char wspolrzedne[] = strzal.toCharArray();
         int j;
@@ -24,10 +26,31 @@ public class Ruch {
     public static void ustawianieStatkow(Plansza plansza, Scanner scanner, int gracz) {
         System.out.println("Gracz " + gracz + " - Rozstaw swoje statki:");
         plansza.drukujPlansze();
-        for (int i = 0; i < 2; i++) {
+        for (int i = 0; i < 4; i++) {
             String statek = scanner.next();
             plansza.ustawStatek(Ruch.wspI(statek), Ruch.wspJ(statek));
 
         }
     }
+
+    public static void strzelanie(Plansza plansza, Scanner scanner, int gracz, int trafienia) {
+        while (true) {
+            System.out.println("Gracz " + gracz + " strzela:");
+            plansza.drukujPlansze();
+            String strzal = scanner.next();
+            if (plansza.getPlansza(Ruch.wspI(strzal), Ruch.wspJ(strzal)) == 'S') {
+                System.out.println("Trafiony!");
+                plansza.zaznaczTrafiony(Ruch.wspI(strzal), Ruch.wspJ(strzal));
+                trafienia++;
+                if (trafienia == 4){
+                    System.out.println("Bum! Wygrales!!! ");
+                }
+            } else {
+                System.out.println("Pudlo!");
+                plansza.zaznaczPudlo(Ruch.wspI(strzal), Ruch.wspJ(strzal));
+                break;
+            }
+        }
+    }
 }
+
